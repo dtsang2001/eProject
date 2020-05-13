7426b6dfaa279e4d6f17f5062e01847c77e639a1
dtsang2001 <thesang201@gmail.com>
2020-05-13 20:50:44 +0700

    Sang update

---
+++ js/sang.js
@@ -0,0 +1,28 @@
+
+var position = $(window).scrollTop(); 
+
+$( () => {
+    $(window).scroll( (e) => {
+
+        var scroll = $(window).scrollTop();
+
+        if(scroll > position && scroll >= 300) {
+            $('#nav-fixed').removeClass('scroll-down-js');
+            $('#nav-fixed').addClass('scroll-up-js');
+        } else {
+            $('#nav-fixed').removeClass('scroll-up-js');
+            $('#nav-fixed').addClass('scroll-down-js');
+        }
+
+        position = scroll;
+
+    });
+
+    $('.close-side').click( () => {
+        $('.side-nav-cart').css({"width" : "0px"});
+    })
+    
+    $('.nav-icon-cart').click( () => {
+        $('.side-nav-cart').css({"width" : "350px"});
+    })
+});
