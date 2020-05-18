36070ff0f308a89878d946920534e0de17fe2ad1
dtsang2001 <thesang201@gmail.com>
2020-05-18 15:00:41 +0700

    sang update

--- js/app.js
+++ js/app.js
@@ -7,7 +7,42 @@ app.controller('myCtrl', ($scope, $http) => {
         $scope.mem = res.data
     });
 
-    $scope.cart = () => {
+    $http.get('storage/products.json').then( (res) => {
+        $scope.product = res.data
+    });
+
+    $scope.carts = [];
+
+    var CartInStorage = sessionStorage.getItem('cart');
+    if (CartInStorage) {
+        $scope.carts = angular.fromJson(CartInStorage);
+    }
+
+    function checkCartExists(Id) {
+        for (var k in $scope.carts) {
+            if ($scope.carts[k].Id == Id) {
+                return k;
+            }
+        }
+        return -1;
+    }
+
+    $scope.add_cart = (pro) => {
+
+        var itemExists = checkCartExists(pro.Id);
+
+        if (itemExists != -1) {
+            $scope.carts[itemExists].quantity += 1;
+        } else {
+            pro.quantity = 1;
+            $scope.carts.push(pro);
+        }
+
+        sessionStorage.setItem('cart', angular.toJson($scope.carts));
+        $scope.cart = pro;
+    }
+
+    $scope.nav_cart = () => {
         $('.side-nav-cart').addClass('size-side');
     }
 
